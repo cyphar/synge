b0ee78dff8fb17137a03c9451900cc9d98b9e725
cyphar <cyphar@cyphar.com>
2013-05-05 11:53:18 +1000

    slight fixed to test suite

--- tests/test.py
+++ tests/test.py
@@ -47,33 +47,35 @@ CASES = [
 #	test expression			expected result		mode	test description
 
 	# expected successes
-	("1+1",				"2",			deg,	"Basic Addition		"),
-	("1+-1",			"0",			deg,	"Convoluted Addition	"),
-	("5-3",				"2",			deg,	"Basic Subtraction	"),
-	("3*4",				"12",			deg,	"Basic Multiplication	"),
-	("4/2",				"2",			deg,	"Basic Division		"),
-	("5%2",				"1",			deg,	"Basic Modulo		"),
-	("15.1%2",			"1.0999999999999996",	deg,	"Basic Modulo		"), # needs to be fixed (due to rounding err)
-	("(1+1)*2",			"4",			deg,	"Basic Parenthesis	"),
-	("10^2",			"100",			deg,	"Basic Indicies		"),
-	("16^(1/4)",			"2",			deg,	"Fractional Indicies	"),
+	("1+1",				"2",			0,	"Basic Addition		"),
+	("1+-1",			"0",			0,	"Convoluted Addition	"),
+	("5-3",				"2",			0,	"Basic Subtraction	"),
+	("0-5-3",			"-8",			0,	"Basic Subtraction	"),
+	("3*4",				"12",			0,	"Basic Multiplication	"),
+	("4/2",				"2",			0,	"Basic Division		"),
+	("5%2",				"1",			0,	"Basic Modulo		"),
+	("15.1%2",			"1.0999999999999996",	0,	"Basic Modulo		"), # needs to be fixed (due to rounding err)
+	("(1+1)*2",			"4",			0,	"Basic Parenthesis	"),
+	("1+(-2)",			"-1",			0,	"Basic Parenthesis	"),
+	("10^2",			"100",			0,	"Basic Indicies		"),
+	("16^(1/4)",			"2",			0,	"Fractional Indicies	"),
 
-	("0xDEADBEEF + 0xA",		"3735928569",		deg,	"Hexadecimal Addition	"),
-	("0xDEADBEEF - 0xA",		"3735928549",		deg,	"Hexadecimal Subtraction	"),
-	("0xA0 / 0xA",			"16",			deg,	"Hexadecimal Division	"),
+	("0xDEADBEEF + 0xA",		"3735928569",		0,	"Hexadecimal Addition	"),
+	("0xDEADBEEF - 0xA",		"3735928549",		0,	"Hexadecimal Subtraction	"),
+	("0xA0 / 0xA",			"16",			0,	"Hexadecimal Division	"),
 
-	("0xDEADBEEF + 10",		"3735928569",		deg,	"Mixed Addition		"),
-	("0xDEADBEEF - 10",		"3735928549",		deg,	"Mixed Subtraction	"),
-	("0xA0 / 10",			"16",			deg,	"Mixed Division		"),
+	("0xDEADBEEF + 10",		"3735928569",		0,	"Mixed Addition		"),
+	("0xDEADBEEF - 10",		"3735928549",		0,	"Mixed Subtraction	"),
+	("0xA0 / 10",			"16",			0,	"Mixed Division		"),
 
-	("3.0+2.1",			"5.0999999999999996",	deg,	"Basic Decimal Notation	"), # needs to be fixed (due to rounding err)
-	("5.3+2",			"7.2999999999999998",	deg,	"Basic Decimal Notation	"), # needs to be fixed (due to rounding err)
-	("0.1+2",			"2.1",			deg,	"Basic Decimal Notation	"),
-	("1.0+3",			"4",			deg,	"Basic Decimal Notation	"),
-	(".1+2",			"2.1",			deg,	"Basic Decimal Notation	"),
-	("1.+3",			"4",			deg,	"Basic Decimal Notation	"),
+	("3.0+2.1",			"5.0999999999999996",	0,	"Basic Decimal Notation	"), # needs to be fixed (due to rounding err)
+	("5.3+2",			"7.2999999999999998",	0,	"Basic Decimal Notation	"), # needs to be fixed (due to rounding err)
+	("0.1+2",			"2.1",			0,	"Basic Decimal Notation	"),
+	("1.0+3",			"4",			0,	"Basic Decimal Notation	"),
+	(".1+2",			"2.1",			0,	"Basic Decimal Notation	"),
+	("1.+3",			"4",			0,	"Basic Decimal Notation	"),
 
-	("log10(100)/2",		"1",			deg,	"Basic Function Division	"),
+	("log10(100)/2",		"1",			0,	"Basic Function Division	"),
 
 	("tan(45)+cos(60)+sin(30)",	"2",			deg,	"Basic Degrees Trig	"),
 	("atan(1)+acos(0.5)+asin(0)",	"105",			deg,	"Basic Degrees Trig	"),
@@ -81,28 +83,28 @@ CASES = [
 	("tan(45)+cos(60)+sin(30)",	"-0.320669413964157",	rad,	"Basic Radian Trig	"),
 	("atan(1)+acos(0.5)+asin(0)",	"1.832595714594046",	rad,	"Basic Radian Trig	"),
 
-	("deg2rad(180/pi)+rad2deg(pi)",	"181",			deg,	"Basic Angle Conversion	"),
+	("deg2rad(180/pi)+rad2deg(pi)",	"181",			0,	"Basic Angle Conversion	"),
 
-	("2^2^2-15-14-12-11-10^5",	"-100036",		deg,	"Complex Expression	"),
+	("2^2^2-15-14-12-11-10^5",	"-100036",		0,	"Complex Expression	"),
 
 	# expected errors
-	("",				errors["empty"],	deg,	"Empty Expression Error	"),
-	(" ",				errors["empty"],	deg,	"Empty Expression Error	"),
-	("does_not_exist",		errors["token"],	deg,	"Unknown Token Error	"),
-	("fake_function()",		errors["token"],	deg,	"Unknown Token Error	"),
-	("fake_function()+23",		errors["token"],	deg,	"Unknown Token Error	"),
-	("1@5",				errors["token"],	deg,	"Unknown Token Error	"),
-	("1/0",				errors["zerodiv"],	deg,	"Zero Division Error	"),
-	("1%(2-(2^2/2))",		errors["zerodiv"],	deg,	"Modulo by Zero Error	"),
-	("1+(1",			errors["parens"],	deg,	"Parenthesis Error	"),
-	("1+4)",			errors["parens"],	deg,	"Parenthesis Error	"),
-	("1+-+4",			errors["numvals"],	deg,	"Token Number Error	"),
-	("2+1-",			errors["numvals"],	deg,	"Token Number Error	"),
-	("abs()",			errors["numvals"],	deg,	"Token Number Error	"),
+	("",				errors["empty"],	0,	"Empty Expression Error	"),
+	(" ",				errors["empty"],	0,	"Empty Expression Error	"),
+	("does_not_exist",		errors["token"],	0,	"Unknown Token Error	"),
+	("fake_function()",		errors["token"],	0,	"Unknown Token Error	"),
+	("fake_function()+23",		errors["token"],	0,	"Unknown Token Error	"),
+	("1@5",				errors["token"],	0,	"Unknown Token Error	"),
+	("1/0",				errors["zerodiv"],	0,	"Zero Division Error	"),
+	("1%(2-(2^2/2))",		errors["zerodiv"],	0,	"Modulo by Zero Error	"),
+	("1+(1",			errors["parens"],	0,	"Parenthesis Error	"),
+	("1+4)",			errors["parens"],	0,	"Parenthesis Error	"),
+	("1+-+4",			errors["numvals"],	0,	"Token Number Error	"),
+	("2+1-",			errors["numvals"],	0,	"Token Number Error	"),
+	("abs()",			errors["numvals"],	0,	"Token Number Error	"),
 ]
 
 def test_calc(program, test, expected, mode, description):
-	command = '%s -m %s "%s"' % (program, mode, test)
+	command = '%s -m "%s" "%s"' % (program, mode, test)
 	output = getstatusoutput(command)[1]
 	if output.replace("\n", "") == expected:
 		print "%s ... %sOK%s" % (description, ansi_good, ansi_reset)
