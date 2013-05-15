7e4dd3d9d8cd8e14a34a23f7a51d454d3d616b4d
Aleksa Sarai <cyphar@cyphar.com>
2013-05-16 01:43:35 +1000

    added "tests" for random numbers

--- tests/test.c
+++ tests/test.c
@@ -37,6 +37,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <strings.h>
 
 #include <synge.h>
@@ -62,6 +63,7 @@ char *bake_args(int argc, char **argv) {
 
 int main(int argc, char **argv) {
 	if(argc < 2) return 1;
+	srand(0); /* seed random number generator, to make it predicatable for testing */
 
 	double result;
 	error_code ecode;
