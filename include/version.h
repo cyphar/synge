0c72d5c5227fab39989999d4428ed3e37e3e8084
cyphar <cyphar@cyphar.com>
2013-08-03 15:24:58 +1000

    core version 1.4.2

--- include/version.h
+++ include/version.h
@@ -24,8 +24,8 @@
 #define __SYNGE_VERSION_H__
 
 /* core version */
-#define __SYNGE_VERSION__ 		"1.4.1"  /* ANSI string */
-#define B__SYNGE_VERSION__ 		 1,4,1,0 /* binary (for resources) */
+#define __SYNGE_VERSION__ 		"1.4.2"  /* ANSI string */
+#define B__SYNGE_VERSION__ 		 1,4,2,0 /* binary (for resources) */
 
 /* cli version */
 #define __SYNGE_CLI_VERSION__ 		"1.1.0"  /* ANSI string */
