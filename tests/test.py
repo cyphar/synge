e84530e91dc602e3e58d0278d2c31e0ecc6b08c7
cyphar <cyphar@cyphar.com>
2013-08-21 21:43:42 +1000

    added unknown base tests

--- tests/test.py
+++ tests/test.py
@@ -364,6 +364,10 @@ CASES = [
 	([""],					[error_get("empty")],		0,	0,		"Empty Expression Error	"),
 	([" "],					[error_get("empty")],		0,	0,		"Empty Expression Error	"),
 
+	(["0s45"],				[error_get("token", 2)],	0,	0,		"Unknown Base Error	"),
+	(["0jk45"],				[error_get("token", 2)],	0,	0,		"Unknown Base Error	"),
+	(["0_45"],				[error_get("token", 2)],	0,	0,		"Unknown Base Error	"),
+
 	(["does_not_exist"],			[error_get("token", 1)],	0,	0,		"Unknown Token Error	"),
 	(["fake_function()"],			[error_get("token", 1)],	0,	0,		"Unknown Token Error	"),
 	(["fake_function()+23"],		[error_get("token", 1)],	0,	0,		"Unknown Token Error	"),
